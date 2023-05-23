5bc9bcc6e43013ce6cf5fb00fc178849a4a7eb5b
ashnichinnusam <ashnichinnusam2003@gmail.com>
2023-05-23 20:28:58 +0530

    Modified

--- script.js
+++
@@ -1,8 +0,0 @@
-var button=document.getElementById('btn');
-button.addEventListener('click',function(){
-    redirectToPage('home.html');
-});
-
-function redirectToPage(pagePath){
-    window.location.href=pagePath;
-}
